05b9b5b96c119914e0f5f77b62f835c4809f127d
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-02-20 02:25:51 +0600

    Fix: Fix logo responsiveness

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <div className="w-full max-w-30 h-10">
+    <div className="w-20 md:w-24 xl:w-30 h-10 transition-all duration-300">
       <Link href="/" className="relative block w-full h-full ">
         <Image
           loading="eager"
           src="/Elvara-logo.png"
           alt="Elvara spaces white logo"
           fill
-          sizes="(max-widtth: 768px) 90px 120px"
+          sizes="(max-width: 768px) 90px 120px"
           className="object-contain"
+          priority
         />
       </Link>
     </div>
