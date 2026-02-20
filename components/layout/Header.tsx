bd1650e739edf5ddfda7d9ffdf9ee2ea80e67424
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-02-20 15:13:41 +0600

    Fix: Margin from top in header section

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,7 +6,7 @@ import MainButton from "../ui/MainButton";
 
 export default function Header() {
   return (
-    <header className="fixed inset-0 z-10 top-6">
+    <header className="fixed inset-0 z-10 top-4">
       <div className="container mx-auto">
         <div className="w-full ps-6 pe-3 py-3 bg-white/15 backdrop-blur-2xl rounded-full border border-(--white-border)">
           <div className="w-full flex items-center justify-between">
