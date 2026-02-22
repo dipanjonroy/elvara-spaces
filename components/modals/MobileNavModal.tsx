514532082fe020721e8d9566fe6fd6c4bbc67daa
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-02-22 16:55:26 +0600

    Feat: Animation added to mobile menu

--- components/modals/MobileNavModal.tsx
+++ components/modals/MobileNavModal.tsx
@@ -2,7 +2,7 @@
 
 import useClickOutside from "@/hooks/useClickOutside";
 import { useModalStore } from "@/store/ModalStore";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Logo from "../ui/Logo";
 import { MdOutlineClose } from "react-icons/md";
 import { useGSAP } from "@gsap/react";
@@ -17,54 +17,57 @@ export default function MobileNavModal() {
 
   useClickOutside(modalRef, closeModal);
 
-  // useGSAP(
-  //   () => {
-  //     if (!modalRef.current) return;
+  useGSAP(() => {
+    if (!modalRef.current) return;
 
-  //     if (isModalOpen && modalId === "mobileMenu") {
-  //       gsap.set(modalRef.current, { display: "block" });
-  //       gsap.fromTo(
-  //         modalRef.current,
-  //         { x: "100%" },
-  //         {
-  //           x: "0%",
-  //           duration: 0.4,
-  //           delay: 0.3,
-  //           ease: "power2.inOut",
-  //         },
-  //       );
-  //     } else {
-  //       gsap.to(modalRef.current, {
-  //         x: "100%",
-  //         duration: 0.4,
-  //         ease: "power2.inOut",
-  //         onComplete: () => {
-  //           gsap.set(modalRef.current, { display: "none" });
-  //         },
-  //       });
-  //     }
-  //   },
-  //   { dependencies: [isModalOpen, modalId] },
-  // );
+    if (isModalOpen && modalId === "mobileMenu") {
+      gsap.fromTo(
+        modalRef.current,
+        { x: "100%", opacity: 0 },
+        {
+          x: "0%",
+          opacity: 1,
+          duration: 0.4,
+          delay: 0.3,
+          ease: "power2.inOut",
+        },
+      );
+    }
+  }, [isModalOpen, modalId]);
+
+  const handleClosModal = () => {
+    if (!modalRef.current) return;
+
+    gsap.to(modalRef.current, {
+      x: "100%",
+      opacity: 0,
+      duration: 0.4,
+      ease: "power2.inOut",
+      onComplete: () => {
+        closeModal();
+      },
+    });
+  };
 
   return (
-    <div
-      ref={modalRef}
-      className={`w-full h-screen bg-(--background) p-8 transform transition-transform duration-300 ease-in-out ${isModalOpen ? "translate-x-0":"translate-x-full"}`}
-    >
+    <div ref={modalRef} className="w-full h-screen bg-(--background) p-8">
       <div className="w-full h-full">
         <div className="flex items-center justify-between pb-4 border-b border-(--black-border) mb-8">
           <Logo variant="black" />
 
-          <button type="button" onClick={closeModal} className="cursor pointer">
+          <button
+            type="button"
+            onClick={handleClosModal}
+            className="curso-pointer"
+          >
             <MdOutlineClose className="text-(--text-color) text-2xl" />
           </button>
         </div>
 
-        <MobileMenu/>
+        <MobileMenu />
 
-        <div className="mt-10">
-          <MainButton onClick={()=>console.log("Main button clicked")}/>
+        <div className="mt-20">
+          <MainButton onClick={() => console.log("Main button clicked")} />
         </div>
       </div>
     </div>
