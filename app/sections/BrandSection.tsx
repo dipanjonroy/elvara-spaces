7bcd4433a01104cc5336e266823f1d281b7817ba
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-02-24 12:56:43 +0600

    Fix: Positions of brand sliders

--- app/sections/BrandSection.tsx
+++ app/sections/BrandSection.tsx
@@ -4,11 +4,14 @@ export default function BrandSection() {
   return (
     <section className="py-20">
       <div className="container mx-auto">
-        <div className="w-full flex flex-col items-center gap-10 xl:gap-18 overflow-hidden">
-          <h2 className="font-semibold leading-8 ">
-            Our Trusted Brands
-          </h2>
-          <div className="w-full lg:w-200 overflow-hidden">
+        <div className="w-full flex flex-col lg:flex-row items-center gap-10 xl:gap-20 overflow-hidden">
+          <div className="w-full lg:w-1/6">
+            <span className="text-xl">
+              Trusted by +50 ambitous
+              <br /> brands worldwide
+            </span>
+          </div>
+          <div className="w-full lg:flex-1 overflow-hidden">
             <BrandSlider />
           </div>
         </div>
