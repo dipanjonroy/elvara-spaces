3970784fa941548894b176322cfa36045cd2c633
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-03-14 01:52:08 +0600

    Project iage url fix

--- lib/projects.ts
+++ lib/projects.ts
@@ -68,11 +68,11 @@ export const projects: ProjectTypes[]=[
     title: "Contemporary Bedroom Redesign",
     category:"Bedroom",
     images:[
-      "/projects/Bedroom/p1/Bedroom-project-1-1.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-2.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-3.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-4.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-5.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-1.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-2.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-3.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-4.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-5.jpg",
     ],
     location:"111 Wellington St, Ottawa, ON, Canada"
   },
