1109c83ada568ec34f81c0e855d6b7c1e0589969
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-03-14 01:45:47 +0600

    Project image url fixed

--- lib/projects.ts
+++ lib/projects.ts
@@ -12,11 +12,11 @@ export const projects: ProjectTypes[]=[
     title: "Cozy Family Home Interior",
     category:"Residential",
     images:[
-      "/projects/Residential/P1/Residential-project-1-1.jpg",
-      "/projects/Residential/P1/Residential-project-1-2.jpg",
-      "/projects/Residential/P1/Residential-project-1-3.jpg",
-      "/projects/Residential/P1/Residential-project-1-4.jpg",
-      "/projects/Residential/P1/Residential-project-1-5.jpg",
+      "/projects/Residential/p1/Residential-project-1-1.jpg",
+      "/projects/Residential/p1/Residential-project-1-2.jpg",
+      "/projects/Residential/p1/Residential-project-1-3.jpg",
+      "/projects/Residential/p1/Residential-project-1-4.jpg",
+      "/projects/Residential/p1/Residential-project-1-5.jpg",
     ],
     location:"120 Front St W, Toronto, ON, Canada"
   },
@@ -68,11 +68,11 @@ export const projects: ProjectTypes[]=[
     title: "Contemporary Bedroom Redesign",
     category:"Bedroom",
     images:[
-      "/projects/Bedroom/p1/Bedroom-project-1-1.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-2.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-3.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-4.jpg",
-      "/projects/Bedroom/p1/Bedroom-project-1-5.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-1.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-2.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-3.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-4.jpg",
+      "/projects/Bedroom/P1/Bedroom-project-1-5.jpg",
     ],
     location:"111 Wellington St, Ottawa, ON, Canada"
   },
