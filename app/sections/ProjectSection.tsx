73820a3438a932b82595ffd0fc6c154a2a97fa57
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-03-15 13:00:44 +0600

    Fix: Adjust padding in ProjectSection for improved layout consistency

--- app/sections/ProjectSection.tsx
+++ app/sections/ProjectSection.tsx
@@ -112,7 +112,7 @@ export default function ProjectSection() {
               </div>
 
               <div className="absolute inset-x-6 md:inset-x-12 bottom-6 md:bottom-12">
-                <div className="w-full md:w-max p-5 bg-(--background) rounded-2xl text-(--text-color) shadow-lg">
+                <div className="w-full md:w-max p-6 bg-(--background) rounded-2xl text-(--text-color) shadow-lg">
                   <span className="text-sm">{item.category}</span>
                   <h3 className="mt-3">{item.title}</h3>
                   <span className="font-light text-sm opacity-70">
