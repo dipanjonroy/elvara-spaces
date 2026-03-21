41257cdad18867fef9dd7caf17ae7b98a90e7b8a
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-03-22 02:09:15 +0600

    Feat: Dynamic metadata added to single project page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,15 +1,38 @@
 import { projects } from "@/lib/projects";
 import SingleProjectHeroSection from "./sections/SingleProjectHeroSection";
 
-export default async function page({params}:{params: Promise<{slug:string}>}) {
-  const {slug} = await params;
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
 
-  const project = projects.find((item)=>item.slug === slug);
+  const project = projects.find(
+    (item) => item.slug.toString() === slug.toString(),
+  );
 
-  if(!project) return;
+  return {
+    title:`${project?.title} | Elvara Spaces`,
+    description:"Project Description Here"
+  }
+}
+
+export default async function page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const project = projects.find(
+    (item) => item.slug.toString() === slug.toString(),
+  );
+
+  if (!project) return;
   return (
     <>
-      <SingleProjectHeroSection project={project}/>
+      <SingleProjectHeroSection project={project} />
     </>
   );
 }
