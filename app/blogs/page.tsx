b648f6dbbeab64ddf6b67adb6b62e8e94d2f9f86
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-04-01 16:37:24 +0600

    Feat: Articles list section initialized to blog page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,9 +1,11 @@
-import BlogHero from "@/app/blogs/sections/BlogHero"
+import BlogHero from "@/app/blogs/sections/BlogHero";
+import BlogArticles from "@/app/blogs/sections/BlogArticles";
 
 export default function page() {
   return (
     <>
-    <BlogHero/>
+      <BlogHero />
+      <BlogArticles/>
     </>
   );
 }
---
+++ app/blogs/sections/BlogArticles/index.tsx
@@ -0,0 +1,9 @@
+export default function ArticlesSection() {
+  return (
+    <section className="py-20 md:py-25 lg:py-30 z-10 bg-(--background) relative">
+      <div className="container mx-auto">
+        All articles card here
+      </div>
+    </section>
+  );
+}
