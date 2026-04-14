88d35dc3af45f4a12d180765b5dd968468ee2186
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-04-15 01:42:09 +0600

    Feat: Admin schema Zod validation added

---
+++ schema/AdminSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const AdminSchema = z.object({
+  username: z
+    .string()
+    .min(1, "Username is requried.")
+    .min(3, { error: "Username must be at least 3 characters." })
+    .trim(),
+  email: z
+    .email({ error: "Invalid email address." })
+    .min(1, { error: "Email is required." }),
+  password: z
+    .string()
+    .min(1, { error: "Password is required." })
+    .min(6, { error: "Password must be at least 6 characters." })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/, {
+      error:
+        "Password must contain at least one letter, one number, and one special character",
+    })
+    .trim(),
+  role: z.string().min(1, "Role is required."),
+});
