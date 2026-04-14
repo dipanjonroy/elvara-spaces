274f28326102d6442825045b69b05166ec32d5ce
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-04-15 01:07:36 +0600

    Feat: Admin model created

---
+++ models/AdminModel.ts
@@ -0,0 +1,31 @@
+import mongoose, {Schema, model, Document} from "mongoose";
+
+interface Admin extends Document {
+  username:string;
+  email:string;
+  password:string;
+  role:"admin"
+};
+
+const adminSchema = new Schema<Admin>({
+  username:{
+    type: String,
+    required: true,
+  },
+  email:{
+    type: String,
+    required: true,
+  },
+  password:{
+    type:String,
+    required:true,
+  },
+  role:{
+    type:String,
+    required:true,
+  },
+},{timestamps:true});
+
+const AdminModel = mongoose.models.Admin as mongoose.Model<Admin> || model<Admin>("Admin", adminSchema);
+
+export default AdminModel;
