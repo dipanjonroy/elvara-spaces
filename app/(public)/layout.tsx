700e7f896c6a2a1a1454dd44600a75b85a56e74e
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-04-26 16:11:54 +0600

    Fix(ui): Footer showing bug fixed

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,7 @@
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import SmoothWrapper from "@/components/provider/SmoothWrapper";
 import type { Metadata } from "next";
-import { Footer } from "react-day-picker";
 
 
 
